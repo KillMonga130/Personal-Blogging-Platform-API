50769d3c4b19c2979fa72442fd5b0f702876c554
Mueletshedzi Moses Mubvafhi <mubvafhimoses813@gmail.com>
2024-07-16 18:59:58 +0200

    Creating the fastify server

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')({ logger: true });
+const mongoose = require('mongoose');
+const routes = require('./routes/articleRoutes');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Register routes
+fastify.register(routes);
+
+// Start server
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    console.log('Server running at http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
