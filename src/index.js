9a696e9153d8e6ad0079e457a3dfd8531b7605d2
Mueletshedzi Moses Mubvafhi <mubvafhimoses813@gmail.com>
2024-07-16 19:26:46 +0200

    finishing touches

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const routes = require('./routes/articleRoutes');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/blog', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect('mongodb://localhost:27017/blog')
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 // Register routes
@@ -15,7 +13,7 @@ fastify.register(routes);
 // Start server
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen({ port: 3000 });
     console.log('Server running at http://localhost:3000');
   } catch (err) {
     fastify.log.error(err);
