4d14c71f58dbab6b308944b547686c0c2c3712dc
Mueletshedzi Moses Mubvafhi <mubvafhimoses813@gmail.com>
2024-07-16 18:57:08 +0200

    define article model

---
+++ src/models/articleModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const ArticleSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    content: { type: String, required: true},
+    tags: [string],
+    publishedDate: { type: Date, default: Date.now}
+});
+
+const Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
